4e773f789237ef66bf6345ff3f2fd82661ea8ebb
yekanaath <139680087+yekanaath@users.noreply.github.com>
2023-08-20 10:25:00 +0530

    Add files via upload

---
+++ DAY 1/PUZHUNT.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int N;
+	cin>>N;
+	if(N>=6 && N<=8)
+	{
+	    cout<<"YES";
+	}
+	else
+	{
+	    cout<<"NO";
+	}
+	
+	return 0;
+}
---
+++ DAY 1/TOP10.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int x;
+        cin>>x;
+     if(x<=10)
+     {
+         cout<<"YES"<<endl;
+     }
+     else
+     {
+         cout<<"NO"<<endl;
+     }
+     
+    }
+	// your code goes here
+	return 0;
+}

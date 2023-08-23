5b50ca2319c46449539315f557ec711b61ac9c8b
Yekanaath <139680087+yekanaath@users.noreply.github.com>
2023-08-23 21:54:15 +0530

    Add files via upload

---
+++ DAY4/REACH_HOME.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+       int d=5*x;
+       if(d>=y)
+       cout<<"YES"<<endl;
+       else if(d<y)
+       cout<<"NO"<<endl;
+
+    }
+	// your code goes here
+	return 0;
+}

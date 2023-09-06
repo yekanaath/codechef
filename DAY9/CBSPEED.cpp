cbfcef127427c967c28b8d3762e7796aef286732
Yekanaath <139680087+yekanaath@users.noreply.github.com>
2023-09-06 17:58:03 +0530

    Add files via upload

---
+++ DAY9/CBSPEED.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin>>x>>y;
+    if(x<y)
+    {
+        cout<<"yes";
+    }
+    else
+    {
+        cout<<"no";
+    }
+    
+	// your code goes here
+	return 0;
+}
---
+++ DAY9/MINHEIGHT.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x,h;
+        cin>>x>>h;
+        if(x>=h)
+        {
+            cout<<"yes"<<endl;
+        }
+        else
+        {
+            cout<<"no"<<endl;
+        }
+    }
+	// your code goes here
+	return 0;
+}
